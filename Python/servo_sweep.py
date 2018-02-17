9e09983b116024921528e41daab1cf55b80a416d
Chris Poole <poolec4@aol.com>
2018-02-17 02:46:42 +0000

    Added servo control

--- Python/servo_sweep.py
+++ Python/servo_sweep.py
@@ -4,6 +4,7 @@
 # License: Public Domain
 from __future__ import division
 import time
+import numpy as np
 
 # Import the PCA9685 module.
 import Adafruit_PCA9685
@@ -20,39 +21,33 @@ pwm = Adafruit_PCA9685.PCA9685()
 #pwm = Adafruit_PCA9685.PCA9685(address=0x41, busnum=2)
 
 # Configure min and max servo pulse lengths
-servo_min = 95  # Min pulse length out of 4096
-servo_max = 565  # Max pulse length out of 4096
-
-# Helper function to make setting a servo pulse width simpler.
-def set_servo_pulse(channel, pulse):
-    pulse_length = 1000000    # 1,000,000 us per second
-    pulse_length //= 60       # 60 Hz
-    print('{0}us per period'.format(pulse_length))
-    pulse_length //= 4096     # 12 bits of resolution
-    print('{0}us per bit'.format(pulse_length))
-    pulse *= 1000
-    pulse //= pulse_length
-    pwm.set_pwm(channel, 0, pulse)
+servo_min = 105  # Min pulse length out of 4096
+servo_max = 650  # Max pulse length out of 4096
 
 # Set frequency to 60hz, good for servos.
 pwm.set_pwm_freq(60)
 
-print('Moving servo on channel 0, press Ctrl-C to quit...')
-while True:
-    # Move servo on channel O between extremes.
-    pwm.set_pwm(0, 0, servo_min)
-    pwm.set_pwm(1, 0, servo_min)
-    pwm.set_pwm(2, 0, servo_min)
-    pwm.set_pwm(3, 0, servo_min)
-    pwm.set_pwm(4, 0, servo_min)
-    pwm.set_pwm(5, 0, servo_min)
-    time.sleep(2)
-    
+init_angle = 120
+servo_angles = [init_angle, init_angle, init_angle, init_angle, init_angle, init_angle] # in degrees
+# servo_angles = np.deg2rad(servo_angles) # in radians
 
-    pwm.set_pwm(0, 0, servo_max)
-    pwm.set_pwm(1, 0, servo_max)
-    pwm.set_pwm(2, 0, servo_max)
-    pwm.set_pwm(3, 0, servo_max)
-    pwm.set_pwm(4, 0, servo_max)
-    pwm.set_pwm(5, 0, servo_max)
-    time.sleep(2)
+ccw = [0,2,4]
+cw = [1,3,5]
+
+servo_pulse = np.zeros(6)
+
+for i in ccw:
+    servo_angles[i] = 270-servo_angles[i]
+    
+for i in range(len(servo_angles)):
+    servo_pulse[i] = (servo_angles[i])/270*(servo_max-servo_min) + servo_min
+    pwm.set_pwm(i, 0, int(round(servo_pulse[i])))
+
+print(servo_pulse)
+
+##print('Moving servo on channel 0, press Ctrl-C to quit...')
+##while True:
+##    # Move servo on channel O between extremes.
+##    for num in cw:
+##        pwm.set_pwm(num, 0, servo_min)
+
