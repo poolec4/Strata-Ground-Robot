6126350ae9cbb628b9e822660f8070732b6bbedc
Chris Poole <poolec4@aol.com>
2018-02-19 23:51:00 +0000

    Added some clarification coments for serial format

--- Arduino/Servo_control_from_py/Servo_control_from_py.ino
+++ Arduino/Servo_control_from_py/Servo_control_from_py.ino
@@ -1,7 +1,20 @@
+/*
+  Written by Chris Poole - Spring 2018
+  
+  
+  Sets servo angles from serial input in the form:
+       "th1,th2,th3,th4,th5,th6\n"
+  where the angle is in degrees.
+ 
+ thi = 0 corresponds to the ith servo in the down position
+ 
+ for servos [1,3,5], positive rotation is in the CW direction
+ for servos [2,4,6], positive rotation is in the CCW direction
+*/
+
 #include <Servo.h> 
 
 Servo servo1, servo2, servo3, servo4, servo5, servo6;  // create servo object to control a servo 
-// a maximum of eight servo objects can be created 
 float theta[6] = {0,0,0,0,0,0};
 float theta_m[6];
 float t;
@@ -14,7 +27,7 @@ float val;
 
 void setup() 
 {   
-  Serial.begin(9600);
+  Serial.begin(115200);
 
   servo1.attach(2,500,2500);  // attaches the servo on pin 9 to the servo object 
   servo2.attach(3,500,2500);
@@ -74,14 +87,12 @@ void loop()
     }
     while(comma < 6); 
   }
-
-  //theta = 135+45*sin(2*PI*0.1*t/1000);
   
   for(int servo_num = 0; servo_num <6; servo_num++)
   {  
     theta_m[servo_num] = map(theta[servo_num],0,270,0,180);
-    Serial.print(theta[i]);
-    Serial.print(", ");
+    //Serial.print(theta[i]);
+    //Serial.print(", ");
   }
   Serial.print("\n");
 
