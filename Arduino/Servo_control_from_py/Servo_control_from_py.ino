7282450d3ca3e3b25f2485390a177a8a72e0cf43
Chris Poole <poolec4@aol.com>
2018-02-21 16:25:15 +0000

    Added serial test from IMU. Changed port and baud rate

--- Arduino/Servo_control_from_py/Servo_control_from_py.ino
+++ Arduino/Servo_control_from_py/Servo_control_from_py.ino
@@ -1,21 +1,24 @@
 /*
   Written by Chris Poole - Spring 2018
-  
-  
-  Sets servo angles from serial input in the form:
-       "th1,th2,th3,th4,th5,th6\n"
-  where the angle is in degrees.
+ 
+ 
+ Sets servo angles from serial input in the form:
+ "th1,th2,th3,th4,th5,th6\n"
+ where the angle is in degrees.
  
  thi = 0 corresponds to the ith servo in the down position
  
- for servos [1,3,5], positive rotation is in the CW direction
- for servos [2,4,6], positive rotation is in the CCW direction
-*/
+ for servos 1, 3, 5 positive rotation is in the CW direction
+ for servos 2, 4, 6 positive rotation is in the CCW direction
+ 
+ In other words, the parser breaks if it gets negative values....
+ */
 
 #include <Servo.h> 
 
 Servo servo1, servo2, servo3, servo4, servo5, servo6;  // create servo object to control a servo 
-float theta[6] = {0,0,0,0,0,0};
+float theta[6] = {
+  0,0,0,0,0,0};
 float theta_m[6];
 float t;
 
@@ -27,7 +30,7 @@ float val;
 
 void setup() 
 {   
-  Serial.begin(115200);
+  Serial.begin(19200);
 
   servo1.attach(2,500,2500);  // attaches the servo on pin 9 to the servo object 
   servo2.attach(3,500,2500);
@@ -81,21 +84,26 @@ void loop()
 
       if(dec_index != -1)
         theta[comma] = theta[comma]*pow(10,dec_index-j);
-        
+      
+      //Serial.print(theta[comma]);
+      //Serial.print(", ");
+
       comma++;
       dec_index = -1;
     }
-    while(comma < 6); 
+    while(comma < 6);
+    //Serial.print("\n"); 
   }
-  
+
   for(int servo_num = 0; servo_num <6; servo_num++)
-  {  
+  { 
     theta_m[servo_num] = map(theta[servo_num],0,270,0,180);
-    //Serial.print(theta[i]);
+
+    //Serial.print(theta[servo_num]);
     //Serial.print(", ");
   }
-  Serial.print("\n");
-
+  //sSerial.print("\n");
+  
   servo1.write(180-theta_m[0]);              
   servo2.write(theta_m[1]);
   servo3.write(180-theta_m[2]);
@@ -103,6 +111,7 @@ void loop()
   servo5.write(180-theta_m[4]);
   servo6.write(theta_m[5]);
 } 
+
 
 
 
