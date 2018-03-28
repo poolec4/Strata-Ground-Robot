f1d829219d3ec113b52a45e9f46b0c225bd0d22a
Scott Barnes <scottgbarnes@gwu.edu>
2018-03-27 20:47:58 -0400

    Update Trajectory Planning

--- Robot/Python/Motion_Planning/motion_plan_sim.py
+++ Robot/Python/Motion_Planning/motion_plan_sim.py
@@ -65,18 +65,93 @@ class robot:
 
         return goal
 
-class search: # A* Search (Greedy)
-    def __init__(start):
-        self.open = start
-        self.closed = []
-        self.cameFrom = []
+class aStar: # A* Search
+    def __init__(start, gridSize, numObjects, maxSize):
+        self.open = open(start)
+        self.closed = closed()
+        self.cameFrom = cameFrom()
+        self.f = 10e6
+        self.g = 0
+        self.h = 10e6
+        self.grid = world(gridSize, numObjects, maxSize)
 
-    def heuristic(a, b):
-        (x1, y1) = a
-        (x2, y2) = b
+    def search(self, start, goal):
+        while self.open.location != goal:
+            current = self.open[self.minF()] # Select current location
+            self.closed.add(current, self.grid) # Add current to closed 
+            self.open.remove(current, self.grid) # Remove current from open
+            self.h = self.heuristic(current, goal) # Calculate predicted fugure cost
+            self.g = self.cameFrom.getValue(current) + self.grid[current[0], current[1]] # calculate cost to get to current (this is incorrect - only checking last cameFrom, should check all)
+            self.f = self.h + self.g # Calculate f
+            self.open = self.open.findNeighbors(current) # Find neighbors
+            
+
+    def heuristic(current, goal):
+        (x1, y1) = current
+        (x2, y2) = goal
         return abs(x1 - x2) + abs(y1 - y2)
 
-    def a_star(grid, start, goal):
+    def cost():
 
+        return g
+
+    def minF(self):
+        ind = np.argmin(np.asarray(self.open.value))
+        return ind
+
+
+class open:
+    def __init__(self, start, grid):
+        self.locations = start
+        self.values = grid[start[0], start[1]]
+
+    def add(self, neighbor):
+        self.locations.append(current)
+        self.values.append(grid[current[0], current[1])
+        return self
+
+    def remove(self, current, grid):
+        self.locations.remove(current)
+        self.values.remove(grid[current[0], current[1]])
+        return self
+
+    def findNeighbors(self, current):
+        for i in (-1, 0, 1):
+            for j in range(-1, 0, 1):
+                if grid[current[0]+i, current[1]+j] not in open:
+                    self.open.locations.append([current[0]+i, current[1]+j])
+                    self.open.values.append(grid[current[0]+i, current[1]+j])
+        return self
+                
+
+
+class closed:
+    def __init__(self):
+        self.locations = []
+        self.values = []
+        
+    def add(self, current, grid):
+        self.locations.append(current)
+        self.values.append(grid[current[0], current[1]])
+        return self
+                           
+
+class cameFrom:
+    def __init__(self, gridSize):
+        self.locations = np.empty(gridSize[0], gridSize[1])
+    
+    def update(self, current, previous, grid):
+        self.locations[current[0], current[1]] = previous
+        return self
+
+    def getValue(self, current, closed):
+        cameFrom = self.locations[current[0], current[1]]
+        ind = cameFrom in closed.locations
+        if not ind:
+            raise ValueError('Could not find cameFrom in closed.locations')
+        value = closed.values[ind]
+        return value
+        
+        
 
 class render:
