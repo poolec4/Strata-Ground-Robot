d54f1d8d33c0ead93ad239980a0be757d84ab66c
poolec4 <poolec4@aol.com>
2018-03-29 14:36:41 -0400

    Added point cloud plane test

--- Robot/MATLAB/point_cloud_test.m
+++ Robot/MATLAB/point_cloud_test.m
@@ -26,3 +26,33 @@ plot3(pcloud(:,1),pcloud(:,2),pcloud(:,3),'k.','MarkerSize',1)
 
 
 % https://www.mathworks.com/help/vision/ref/pcfitplane.html
+
+%%
+close all
+clear all
+clc
+
+load('object3d.mat')
+figure
+pcshow(ptCloud)
+xlabel('X(m)')
+ylabel('Y(m)')
+zlabel('Z(m)')
+title('Original Point Cloud')
+
+maxDistance = 0.02;
+referenceVector = [0,0,1];
+maxAngularDistance = 5;
+[model1,inlierIndices,outlierIndices] = pcfitplane(ptCloud,maxDistance,referenceVector,maxAngularDistance);
+plane1 = select(ptCloud,inlierIndices);
+remainPtCloud = select(ptCloud,outlierIndices);
+
+figure
+pcshow(plane1)
+title('Ground Plane')
+
+
+
+
+
+
