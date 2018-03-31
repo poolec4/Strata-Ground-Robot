742a6f06c3c121a5bc6c82e53fca14ea3e1225b7
poolec4 <poolec4@aol.com>
2018-03-31 22:42:34 +0000

    Fixed arduino parse buffer

--- Robot/Python/robot_control/main.py
+++ Robot/Python/robot_control/main.py
@@ -5,7 +5,7 @@ import numpy as np
 
 motor_val = np.asarray([0,0,0,0,0,0])
 
-ardu_ser = serial.Serial('/dev/ttyACM0', 19200)
+ardu_ser = serial.Serial('/dev/ttyACM1', 19200)
 print(ardu_ser)
 buffer = 'A='+str(motor_val[0])+'&B='+str(-motor_val[1])+'&C='+str(motor_val[2])+'&D='+str(-motor_val[3])+'&E='+str(motor_val[4])+'&F='+str(-motor_val[5])+'\n'
 ardu_ser.write(buffer)
@@ -13,7 +13,7 @@ ardu_ser.write(buffer)
 time.sleep(1.0)
 t_init = time.time()
 
-while (time.time()-t_init) < 10.0: 
+while (time.time()-t_init) < 2.0: 
 	motor_val = 300*np.ones((1,6))
 	buffer = 'A='+str(motor_val[0])+'&B='+str(-motor_val[1])+'&C='+str(motor_val[2])+'&D='+str(-motor_val[3])+'&E='+str(motor_val[4])+'&F='+str(-motor_val[5])+'\n'
 	ardu_ser.write(buffer)
