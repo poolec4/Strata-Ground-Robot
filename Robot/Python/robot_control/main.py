4040372fd82f1e18d1142007b4a1551901319492
Chris Poole <poolec4@aol.com>
2018-04-04 18:53:22 -0400

    Fixed key exit

--- Robot/Python/robot_control/main.py
+++ Robot/Python/robot_control/main.py
@@ -17,7 +17,6 @@ x_g = [0, 0];
 th_g = 0;
 
 vicon = vicon(TCP_IP, TCP_PORT, BUFFER_SIZE)
-
 robot = robot()
 
 robot.open('/dev/ttyACM0', 19200)
@@ -28,12 +27,17 @@ t_init = time.time()
 
 RUN_ROBOT = True
 
-while (RUN_ROBOT == True):
-	vicon.get_state();
+try:
+	while (RUN_ROBOT == True):
+		vicon.get_state();
 
-	robot = robot.P_control(vicon.x_v, vicon.q_v)
-	robot = robot.write_motors()
+		robot = robot.P_control(vicon.x_v, vicon.q_v)
+		robot = robot.write_motors()
 
-	time.sleep(0.05)
+		time.sleep(0.05)
 
-robot.stop_robot()
+		if(time.time() - t_init > 100.0):
+			RUN_ROBOT = False
+
+except KeyboardInterrupt:
+	robot.stop_robot()
