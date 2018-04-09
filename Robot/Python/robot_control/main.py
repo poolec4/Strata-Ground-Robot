adbd461d0a27ce32db419c3ee45a1bb10c724474
Chris Poole <poolec4@aol.com>
2018-04-09 16:21:08 -0400

    Tested PI control in software. Reorganized robot class

--- Robot/Python/robot_control/main.py
+++ Robot/Python/robot_control/main.py
@@ -9,8 +9,8 @@ TCP_IP = '192.168.10.7'
 TCP_PORT = 50000
 BUFFER_SIZE = 1024
 
-x_g = [-3,3]
-th_g = -math.pi
+x_g = [2, 2]
+th_g = 0
 
 vicon = Vicon(TCP_IP, TCP_PORT, BUFFER_SIZE)
 robot = Robot()
@@ -29,11 +29,12 @@ try:
 	while RUN_ROBOT:
 		print("\n")
 		
-		if (time.time() - t_vicon) > 0.2:
+		if (time.time() - t_vicon) > 0.1:
 			vicon.get_state()
 			t_vicon = time.time()
 
-		robot = robot.P_control(vicon.x_v, vicon.q_v)
+#		robot = robot.P_control(vicon.x_v, vicon.q_v)
+		robot = robot.PI_control(vicon.x_v, vicon.q_v, 0.9)
 		
 		if (time.time() - t_send) > 0.1:
 			robot = robot.write_motors()
@@ -41,7 +42,7 @@ try:
 
 		if (time.time() - t_init) > 1000.0:
 			RUN_ROBOT = False
-#		time.sleep(0.5)
+#		time.sleep(1)
 
 except KeyboardInterrupt:
 	robot.stop_robot()
--- Robot/Python/robot_control/robot.py
+++ Robot/Python/robot_control/robot.py
@@ -3,29 +3,6 @@ import serial
 import math
 import numpy as np
 
-def quat_to_eangles(quat):
-	# quat = [qx qy qz qw]
-	
-	q1 = quat[0]
-	q2 = quat[1]
-	q3 = quat[2]
-	q0 = quat[3]
-	
-	phi = math.atan2(2*(q0*q1 + q2*q3), 1 - 2*(q1**2 + q2**2))
-	theta = math.asin(2*(q0*q2 - q3*q1))
-	psi = math.atan2(2*(q0*q3 + q1*q2), 1 - 2*(q2**2 + q3**2))
-
-	eangles = [phi, theta, psi] # [roll, pitch, yaw]
-	return eangles
-
-def remap_angle(th)
-	if th > math.pi:
-		th_remap = -(2*math.pi - th)
-	if a < -math.pi:
-		th_remap =  2*math.pi + th
-	
-	return th_remap
-
 class Robot:
 	def __init__(self, min_motor_speed=50, max_motor_speed=255):
 		self.motor_vals = np.zeros(6)
@@ -35,7 +12,7 @@ class Robot:
 		self.kp = 3  # kp>0
 		self.ka = 50 # kb<0
 		self.kb = -40 # ka-kb>0
-		self.kpi = 0  # kp>0
+		self.kpi = 5  # kp>0
 		self.kai = 0 # kb<0
 		self.kbi = 0 # ka-kb>0
 
@@ -66,16 +43,15 @@ class Robot:
 	def bound_motor_speed(self, v):
 		if v > self.max_motor_speed:
 			v_bounded = self.max_motor_speed
-
-		if v < self.min_motor_speed and v > 10:
+		elif v < self.min_motor_speed and v > 10:
 			v_bounded = self.min_motor_speed
-
-		if v < -self.max_motor_speed:
+		elif v < -self.max_motor_speed:
 			v_bounded = -self.max_motor_speed
-
-		if v > -self.min_motor_speed and v < -10:
+		elif v > -self.min_motor_speed and v < -10:
 			v_bounded = -self.min_motor_speed
-		
+		else:
+			v_bounded =	v
+
 		return v_bounded
 
 	def P_control(self, x_v, q_v):
@@ -112,10 +88,10 @@ class Robot:
 
 	def PI_control(self, x_v, q_v, rolloff_fac):
 
-		dt = time.time() - t_old
+		dt = time.time() - self.t_old
 
 		eangles = quat_to_eangles(q_v)
-		th = -eangles[2] 
+		th = eangles[2] 
 		dx = self.x_g - x_v[0]
 		dy = self.y_g - x_v[1]
 		print("xv=" + str(x_v[0]) + "yv=" + str(x_v[1]))
@@ -123,7 +99,7 @@ class Robot:
 		
 		p = math.sqrt(dx**2 + dy**2)
 		a = -th + math.atan2(dy,dx)
-		b = -th - a
+		b = -th - a + self.th_g
 
 		self.p_sum = rolloff_fac*self.p_sum + p*dt
 		self.a_sum = rolloff_fac*self.a_sum + a*dt
@@ -135,10 +111,10 @@ class Robot:
 		self.b_sum = remap_angle(self.b_sum)
 
 		print("p=" + str(p) + ", a=" + str(a) + ", b=" + str(b))
-		print("p_sum=" + str(self.p_sum) + ", a_sum=" + str(self.a_sum) + ", b_sum=" + str(self.b_sum))
+		print("dt=" + str(dt) + "\np_sum=" + str(self.p_sum) + ", a_sum=" + str(self.a_sum) + ", b_sum=" + str(self.b_sum))
 
 		v = self.kp*p + self.kpi*self.p_sum
-		omega = self.ka*a + self.kb*b + self.kai*a_sum + self.kbi*b_sum
+		omega = self.ka*a + self.kb*b + self.kai*self.a_sum + self.kbi*self.b_sum
 
 		v_r = (2*v + omega*self.L)/(2*self.R)
 		v_l = (2*v - omega*self.L)/(2*self.R)
@@ -151,7 +127,7 @@ class Robot:
 		self.motor_vals[0:3] = v_l
 		self.motor_vals[3:6] = v_r
 
-		t_old = time.time
+		self.t_old = time.time()
 		return self
 
 	def write_motors(self):
@@ -167,3 +143,27 @@ class Robot:
 		buffer = "Z#\n"
 		self.ardu_ser.write(buffer)
 		return self
+
+def quat_to_eangles(quat):
+	# quat = [qx qy qz qw]
+	qx = quat[0]
+	qy = quat[1]
+	qz = quat[2]
+	qw = quat[3]
+	
+	phi = math.atan2(2*(qw*qx + qy*qz), 1 - 2*(qx**2 + qy**2))
+	theta = math.asin(2*(qw*qy - qz*qx))
+	psi = math.atan2(2*(qw*qz + qx*qy), 1 - 2*(qy**2 + qz**2))
+
+	eangles = [phi, theta, psi] # [roll, pitch, yaw]
+	return eangles
+
+def remap_angle(th):
+	if th > math.pi:
+		th_remap = -(2*math.pi - th)
+	elif th < -math.pi:
+		th_remap =  2*math.pi + th
+	else:
+		th_remap = th
+	
+	return th_remap
