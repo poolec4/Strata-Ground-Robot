889b56e1a257bfcceb67521c2b13104625e2ecc7
Scott Barnes <scottgbarnes@gwu.edu>
2018-04-22 17:04:11 -0400

    Added Platform Dead Zone

--- Platform/Python/main.py
+++ Platform/Python/main.py
@@ -22,7 +22,7 @@ run_controller = True
 
 init_angle = 135.0 # in degrees
 servo_angles = init_angle*np.asarray([1,-1,1,-1,1,-1])
-      
+dead_zone = 2 # Angle in degrees
 
 # Init controller
 controller = controller(init_angle=init_angle, version='v1.0', bounds=(105, 170))
@@ -64,7 +64,7 @@ first = True
 while run_controller == True:
     # time.sleep(0.5)
 ## Parse IMU values in the form "#A=a1,a2,a3#E=y,p,r\n"
-    
+
 ##    Traceback (most recent call last):
 ##  File "main.py", line 62, in <module>
 ##    accel = temp.astype(np.float)
@@ -76,7 +76,7 @@ while run_controller == True:
     # print(imu_data)
     imu_data = imu_data.rstrip()
     imu_data = filter(None,imu_data.split('#'))
-    
+
     for i in range(len(imu_data)):
         if "A=" in imu_data[i]:
             temp = imu_data[i].replace("A=","")
@@ -87,14 +87,14 @@ while run_controller == True:
                 # print(temp)
                 # print('Invalid Input')
                 #time.delay(1)
-            
+
         if "E=" in imu_data[i]:
             temp = imu_data[i].replace("E=","")
             temp = np.array(temp.split(','))
             if (is_number(temp[0]) == True and is_number(temp[1]) == True and is_number(temp[2]) == True):
                 e_angles = np.asarray([float(temp[0]), float(temp[1]), float(temp[2])])
-            
-            
+
+
 ##Traceback (most recent call last):
 ##  File "main.py", line 71, in <module>
 ##    if (is_number(temp[0]) == True and is_number(temp[1]) == True and is_number(temp[2]) == True):
@@ -104,10 +104,10 @@ while run_controller == True:
                 # print(temp)
                 # print('Invalid Input')
                 # time.delay(1)
-    
+
     # print("Accel = "+str(accel))
     # print("Eangles = "+str(e_angles))
-    ## Lookup translation given current servo angles    
+    ## Lookup translation given current servo angles
     if count > max_count:
         run_controller = False
     if count > 2 and first==True:
@@ -120,7 +120,7 @@ while run_controller == True:
         print('Intial Eangles')
         print(init_eangles)
         first = False
-        
+
     if first == False:
         ##  Controller
         if e_angles[2] > 0:
@@ -133,31 +133,31 @@ while run_controller == True:
         orientation = np.asarray([e_angles[1]-init_eangles[1], e_angles[2]-init_eangles[2], -(e_angles[0]-init_eangles[0])]) # whats the order?
         print('Platform Orientation: ')
         print(orientation)
-        controller = controller.step(orientation, np.asarray([0.0, 0.0, 0.0]), t)
-        servo_angles = np.asarray([controller.theta[0], controller.theta[1], controller.theta[2], controller.theta[3], controller.theta[4], controller.theta[5]]) # This will be returned in degrees
-        servo_data[int(count-1), :] = servo_angles.reshape(6)
-        accel_data[int(count-1)] = orientation.reshape(3)
-        t_data[count-1] = time.time()-t_init
-        # print('Controller Angles: ')
-        # print(controller.theta)
-        # print('Servo Angles: ')
-        # print(servo_angles)
-        ## Set servos via serial buffer to Arduino
-        servo_write = servo_angles
-        for num in [1,3,5]:
-             servo_write[num] = -servo_angles[num] # convert all servo angles to positive (-1*[1,3,5])
-        
-        ## Test angle definition
-        ##servo_write = 135+np.ones(6)*30*np.sin(2*math.pi*0.2*t)
-            
-        servo_write = np.around(servo_write,1)
-        buffer = 'A='+str(servo_write[0][0])+'&B='+str(servo_write[1][0])+'&C='+str(servo_write[2][0])+'&D='+str(servo_write[3][0])+'&E='+str(servo_write[4][0])+'&F='+str(servo_write[5][0])+'\n'
-        #import pdb; pdb.set_trace()
-        ardu_ser.write(buffer)
-        print('Servo Angles: ')
-        print(buffer)
-        print("Time Elapsed: " + str(time.time()-tic))
-        print(count)
+        if np.max(abs(orientation)) > dead_zone:
+            controller = controller.step(orientation, np.asarray([0.0, 0.0, 0.0]), t)
+            servo_angles = np.asarray([controller.theta[0], controller.theta[1], controller.theta[2], controller.theta[3], controller.theta[4], controller.theta[5]]) # This will be returned in degrees
+            servo_data[int(count-1), :] = servo_angles.reshape(6)
+            accel_data[int(count-1)] = orientation.reshape(3)
+            t_data[count-1] = time.time()-t_init
+            # print('Controller Angles: ')
+            # print(controller.theta)
+            # print('Servo Angles: ')
+            # print(servo_angles)
+            ## Set servos via serial buffer to Arduino
+            servo_write = servo_angles
+            for num in [1,3,5]:
+                 servo_write[num] = -servo_angles[num] # convert all servo angles to positive (-1*[1,3,5])
+
+            ## Test angle definition
+            ##servo_write = 135+np.ones(6)*30*np.sin(2*math.pi*0.2*t)
+            servo_write = np.around(servo_write,1)
+            buffer = 'A='+str(servo_write[0][0])+'&B='+str(servo_write[1][0])+'&C='+str(servo_write[2][0])+'&D='+str(servo_write[3][0])+'&E='+str(servo_write[4][0])+'&F='+str(servo_write[5][0])+'\n'
+            #import pdb; pdb.set_trace()
+            ardu_ser.write(buffer)
+            print('Servo Angles: ')
+            print(buffer)
+            print("Time Elapsed: " + str(time.time()-tic))
+            print(count)
 ##Traceback (most recent call last):
 ##  File "main.py", line 71, in <module>
 ##  if (is_number(temp[0]) == True and is_number(temp[1]) == True and is_number(temp[2]) == True):
